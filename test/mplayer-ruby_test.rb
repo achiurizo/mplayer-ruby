9d6153e1a220f772a9f8f82cdc96e0da7cf28502
achiu <mr.arthur.chiu@gmail.com>
2010-01-23 20:00:16 -0800

    initial test preparations

--- test/mplayer-ruby_test.rb
+++ test/mplayer-ruby_test.rb
@@ -1,4 +1,4 @@
-require 'teststrap'
+require File.expand_path("teststrap", File.dirname(__FILE__))
 
 context "mplayer-ruby" do
   setup do
--- test/teststrap.rb
+++ test/teststrap.rb
@@ -1,3 +1,8 @@
 require 'rubygems'
 require 'riot'
+require 'rr'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'mplayer-ruby'
+
+
+Riot::Situation.instance_eval { include RR::Adapters::RRMethods }
