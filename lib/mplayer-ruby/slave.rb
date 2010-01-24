6f632c4e7490472a20737155261a1e09f1468899
achiu <mr.arthur.chiu@gmail.com>
2010-01-24 08:45:12 -0800

    including comments with the code

--- lib/mplayer-ruby/slave.rb
+++ lib/mplayer-ruby/slave.rb
@@ -9,18 +9,22 @@ module MPlayer
       @pid,@stdin,@stdout,@stderr = Open4.popen4(mplayer)
     end
 
+    # Increase/decrease volume
+    # :up increase volume
+    # :down decreases volume
+    # :set sets the volume at <value>
     def volume(action,value=30)
       cmd =
       case action
       when :up then "volume 1"
       when :down then "volume 0"
       when :set then "volume #{value} 1"
-      else
-        return false
+      else return false
       end
       send cmd
     end
 
+    # Returns a hash of the meta information of the file.
     def meta_info
       meta = "get_meta_%s"
       album = send meta('album')
@@ -33,6 +37,10 @@ module MPlayer
       {:album => album,:artist => artist, :comment => comment, :genre => genre, :title => title, :track => track, :year => year}
     end
 
+    # Seek to some place in the file
+    # :relative is a relative seek of +/- <value> seconds (default).
+    # :perecent is a seek to <value> % in the file.
+    # :absolute is a seek to an absolute position of <value> seconds.
     def seek(value,type = :relative)
       cmd = case type
       when :percent then "seek #{value} 1"
@@ -41,7 +49,10 @@ module MPlayer
       end
       send cmd
     end
-
+    
+    # Set/adjust the audio delay.
+    # If type is :relative adjust the delay by <value> seconds.
+    # If type is :absolute, set the delay to <value> seconds.     
     def audio_delay(value,type = :relative)
       type == :relative ? send("audio_delay #{value} 0") : send("audio_delay #{value} 1")
     end
@@ -49,8 +60,10 @@ module MPlayer
     # Write the current position into the EDL file.
     def edl_mark; send("edl_mark"); end
 
+    # Pauses/Unpauses the file.
     def pause; send("pause") ; end
 
+    # Quits MPlayer
     def quit; send('quit') ; end
 
 
